0222ffb899c16c55c733f2a6fd6ec9a85848101d
Katya <KatiaKaterina3@yandex.ru>
2022-01-26 20:11:37 +0300

    Changed my1.py and readme.md/Изменен my1.py и readme.md

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # MyFirstRepository
+Description/Описание
+-----
 Это мой первый репозиторий в GitHub.
 Здесь буду выкладывать свои программы.
 
 This is my first GitHub repository.
 Here I will post my programs.
+Programs/Программы
+-----
+my1.py - Guess the number/Угадай число
--- my1.py
+++ my1.py
@@ -1,2 +1,20 @@
-def hello(name):
-    print(f"Hello, {name}!")
+# Угадай число/Guess the number
+import random
+
+
+print("\tWelcome to guess the number game!")
+print("\nI guessed a natural number from the range from 1 to 100.")
+print("Try to guess it in the minimum number of attempts.\n")
+the_number = random.randint(1, 100)
+guess = int(input("Your guess: "))
+tries = 0
+while guess != the_number:
+    if guess > the_number:
+        print("Less...")
+    else:
+        print("More...")
+    guess = int(input("Your guess: "))
+    tries += 1
+print("You managed to guess the number! It's really", the_number)
+print("You spent only", tries, "attempts to guess!\n")
+input("\n\nPress Enter to exit.")
